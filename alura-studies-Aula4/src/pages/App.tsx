6ada43cde2884b528c4ca65383045148738feb6f
GabrielSantosoliveira <gabrielbh@hotmail.com>
2022-04-22 04:22:13 +0000

    a

---
+++ alura-studies-Aula4/src/pages/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Cronometro from '../components/Cronometro';
+import Formulario from '../components/Formulario';
+import Lista from '../components/Lista';
+import style from './App.module.scss';
+import {useState} from 'react';
+import { Itarefa } from '../types/tarefa';
+
+function App() {
+  const [tarefas,setTarefas] = useState<Itarefa[] | []>( [] )
+
+  return (
+    <div className={style.AppStyle}>
+      <Formulario setTarefas={setTarefas}/>
+      <Lista tarefas={tarefas}/>
+      <Cronometro />
+    </div>
+  );
+}
+
+export default App;
